ae101413ce0628cd00560dbb995d1ef6bb160fba
ng356du476gd <nmdcnn@gmail.com>
2019-09-24 03:47:02 -0700

    successfully make single selection between two radio button. successfully convert and display number in mile or km.

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -6,12 +6,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 
 
 
 
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioButton m2km,km2m;
+    RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +26,33 @@ public class MainActivity extends AppCompatActivity {
 
         final EditText inputText = findViewById(R.id.editText);
         final TextView outputText = findViewById(R.id.textView1);
+
+        m2km = (RadioButton)findViewById(R.id.radioButton);
+        km2m = (RadioButton)findViewById(R.id.radioButton2);
+        radioGroup = (RadioGroup)findViewById(R.id.RGroup);
+
+
+
         Button Add = findViewById(R.id.bt_add);
 
         Add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
 
-                double convertedNumber = (Double.valueOf(inputText.getText().toString()))/2;
-                String stringdouble= Double.toString(convertedNumber);
+                if(m2km.isChecked()) {
 
-                outputText.setText(stringdouble);
+                    double convertedNumber = (Double.valueOf(inputText.getText().toString())) * 1.6;
+                    String stringdouble = Double.toString(convertedNumber);
+
+                    outputText.setText(stringdouble + " KM");
+                }else{
+                    double convertedNumber = (Double.valueOf(inputText.getText().toString())) / 1.6;
+                    String stringdouble = Double.toString(convertedNumber);
+
+                    outputText.setText(stringdouble + " MI");
+                }
+
+
 
             }
         });
